b752cfa515090c5c5ee0ac6411f0e744f2e8e9e6
Nowal <nowalplayer@gmail.com>
2015-10-31 00:29:56 +0100

    Add body to index

--- View/index.php
+++ View/index.php
@@ -6,7 +6,8 @@
       <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
       <meta name="author" content="konrad">
       <title>Fluffy | Home</title>
-      <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" type="text/css">
+      <link rel="stylesheet" href="css/default.css" type="text/css">
       <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
       <meta name="keywords" content="world,facebook,face,faces,compare,hotornot,family,fun">
       <meta name="description" content=":::">
@@ -22,5 +23,46 @@
   <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
 
+     <style type="text/css">
+     a{
+       color: #fff;
+     }
+     .init{
+       background: url('../images/bg.png') repeat-x;
+       padding: 0 0 30px 0;
+     }
+    </style>
   </head>
+
+  <body>
+    <div class="container-fluid init">
+      <div class="container">
+        <header class="row">
+          <div class="col-md-12">
+            <div class="row">
+              <div class="col-md-12">
+            <h1 class="text-center"><a href="#">Fluffy</a></h1>
+          </div>
+          <div class="col-md-6 col-sm-6 col-xs-6 col-xs-offset-3 col-sm-offset-3 col-md-offset-3">
+            <form class="form-horizontal" method="get" action="index.php">
+              <div class="form-group">
+                <div class="input-group">
+                  <span class="input-group-btn">
+                    <input type="text" class="form-control" name="search">
+                    <button class="btn btn-info">Search</button>
+                  </span>
+                </div>
+              </div>
+            </form>
+          </div>
+          </div>
+          </div>
+
+
+
+
+        </header>
+      </div>
+    </div>
+  </body>
 </html>
