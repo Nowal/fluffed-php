dec0e7b9c580b8ecd36794990de93edd121da35e
Nowal <nowalplayer@gmail.com>
2015-10-30 22:41:48 +0100

    Add view

---
+++ View/index.php
@@ -0,0 +1,12 @@
+<!Doctype html>
+<html lang="en">
+  <head>
+      <meta http-equiv="x-ua-compatible" content="ie=edge,chrome=1">
+      <meta http-equiv="content-type" content="text/html;charset=utf-8">
+      <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+      <title>Fluffy</title>
+
+      <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+      <link rel=
+  </head>
+</html>
