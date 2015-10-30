f3d6950f8d2d47a63097191b46a2a9f7129a8cc5
Nowal <nowalplayer@gmail.com>
2015-10-30 23:06:50 +0100

    Add head

--- View/index.php
+++ View/index.php
@@ -1,12 +1,26 @@
 <!Doctype html>
-<html lang="en">
+<html lang="en" prefix="www.ogp.me/ns#">
   <head>
       <meta http-equiv="x-ua-compatible" content="ie=edge,chrome=1">
       <meta http-equiv="content-type" content="text/html;charset=utf-8">
       <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
-      <title>Fluffy</title>
-
+      <meta name="author" content="konrad">
+      <title>Fluffy | Home</title>
       <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
-      <link rel=
+      <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+      <meta name="keywords" content="world,facebook,face,faces,compare,hotornot,family,fun">
+      <meta name="description" content=":::">
+      <meta name="abstract" content="::">
+      <meta name="robots" content="noopd,noydir">
+
+      <meta proprety="og:title" content="Fluffy | Home">
+      <meta proprety="og:url" content="http://fluffy.com/">
+      <meta proprety="og:image" content="og.png">
+      <meta proprety="og:description" content="...">
+
+      <!--[if IE]>
+  <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
   </head>
 </html>
