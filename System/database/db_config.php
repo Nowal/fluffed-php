62e87ac720abd8cba7e5f07249484edd0aa0f9d4
Nowal <nowalplayer@gmail.com>
2015-10-31 10:20:51 +0100

    Adding code of db_config.php & View/index.php

---
+++ System/database/db_config.php
@@ -0,0 +1,17 @@
+<?php
+
+$db_info = [];
+
+$db_info['dsn']       = 'localhost';
+$db_info['database']  = 'database';
+$db_info['user']      = 'root';
+$db_info['password']  = '';
+
+if(array_count_values($db_info) == 4)
+{
+    $db = new __CORE();
+    foreach($db_info AS $key => $value)
+    {
+        $db->$key = $value; // Like a boss
+    }
+}
